14d1b4ce9e88262eb919bf28f5a120b43af19d9f
vagrant <vagrant@ubuntu1804.localdomain>
2019-11-07 08:50:04 -0800

    exercises python

---
+++ 09_exercise/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+def mensaje_inicio():
+    mensaje = '\nInicio de Programa\n'
+    print(mensaje)
+    
+def mensaje_fin():
+    mensaje = '\nFin de Programa\n'
+    print(mensaje)
+        
+def ingrese_texto(mensaje = None):
+    texto = ''
+    if(mensaje is None):
+        mensaje = "Ingrese un texto: "
+        
+    try:
+        texto = raw_input(mensaje + "\t")                    ## esto se usa para string
+    except: 
+        texto = ingrese_texto(mensaje)
+        
+    return texto
+
+def ingrese_numero(mensaje = None):
+    numero = 0
+    if(mensaje is None):
+        mensaje = "Ingrese un numero: "
+        
+    try:
+        numero = float(input(mensaje + "\t"))
+    except: 
+        numero = ingrese_numero(mensaje)
+        
+    return numero
+
+def factorial(numero):
+    if numero == 1:
+        return numero
+    return numero*factorial(numero-1)
+
+## Inicia programa aca
+
+mensaje_inicio()
+
+numero     =   ingrese_numero("Ingrese un numero: ")
+
+respuesta = factorial(numero)
+
+
+mensaje_resultado = '\nEl resultado es : \t \n\t'
+
+print(mensaje_resultado)
+
+print(respuesta)
+
+mensaje_fin()    
