fcfe8cc7cb8de8e56056e20fa7d9dfcea8b8332a
khrist14n <khrist14n@gmail.com>
2019-11-17 19:55:42 +0000

    cambios

--- 13_exercise/main.py
+++ 13_exercise/main.py
@@ -42,41 +42,33 @@ def es_vocal( letra ):
             respuesta = True
     return respuesta
 
-def es_no_vocal( clave ):
-    letras_vocales = 'asdasd'
+def es_no_vocal( letra ):
+    letras_vocales = 'aeiou'
     respuesta = True
     for letra_vocal in letras_vocales:
         if(letra == letra_vocal):
             respuesta = False
     return respuesta
 
+def ingreso_letra():
+    letra = ingrese_texto('Ingrese Un Caracter')
+    while len(letra)>1:
+        letra = ingrese_texto('Ingrese Un Caracter')
+    
+    vocal = es_vocal(letra)
+    if vocal == True:
+        print('Es Vocal')
+    else:
+        print('No es Vocal')
+    return letra    
+
 ## Inicia programa aca
 
 mensaje_inicio()
 
-usuario     =   ingrese_texto("Ingrese el usuario: ")
-clave       =   ingrese_texto("Ingrese la clave: ")
+letra = ingreso_letra()
 
-usuario_validado    = es_usuario(usuario)
-clave_validada      = es_clave(clave)
-
-mensaje_validado                 = '\nSi es un usuario valido\n'
-mensaje_no_validado              = '\nNo es un usuario valido\n'
-mensaje_no_determinado           = '\nNo se puede determinar\n'
-
-mensaje_usuario = '\nEl usuario ingresado es %s \n'%(usuario)
-mensaje_clave = '\nLa clave ingresada es %s \n'%(clave)
-
-print(mensaje_usuario)
-print(mensaje_clave)
-
-mensaje_resultado = 'El resultado de la validacion es : \t '
-
-print(mensaje_resultado)
-
-if      (usuario_validado == True and clave_validada==True):
-    print(mensaje_validado)
-else:
-    print(mensaje_no_validado)
+while letra == ' ':
+    letra = ingreso_letra()
 
 mensaje_fin()    
